4e17f923d53f82f6fa9ab06971f73f41b6ff919f
Ben Mart <bmart392@gmail.com>
2023-10-23 18:22:48 -0400

    modified callbacks and took image centering back out

--- scripts/RobotControlNode.py
+++ scripts/RobotControlNode.py
@@ -8,11 +8,12 @@ File containing the RobotControlNode class.
 from rospy import is_shutdown, init_node, Rate, Publisher, Subscriber
 from geometry_msgs.msg import TwistStamped, PoseStamped, WrenchStamped
 from franka_msgs.msg import FrankaState
-from rv_msgs.msg import ManipulatorState
-from std_msgs.msg import Float64, String, Bool
+# from rv_msgs.msg import ManipulatorState
+from std_msgs.msg import Float64, Bool
 
 # Import standard packages
-from numpy import sign, zeros, array, sum, sqrt, mean, median, append, arange
+from numpy import zeros, array, median, append, arange
+
 
 # TODO - High - Verify position error is being properly received
 
@@ -31,12 +32,12 @@ class RobotControlNode:
         # Define variables to store data for force history
         self.force_history_length = 30
         self.force_history = [array([]), array([]), array([]), array([]), array([]), array([])]
-        
+
         # Define control inputs to be used
         self.image_based_control_input = zeros(6)
         self.force_based_control_input = zeros(6)
         self.position_based_control_input = zeros(6)
-        
+
         # Define variables to store relevant robot information
         self.robot_sensed_force = zeros(6)
         self.current_pose = zeros(6)
@@ -44,19 +45,16 @@ class RobotControlNode:
         # Define variables to store desired motion
         self.goal_pose = zeros(6)
         self.goal_force = zeros(6)
-        
+
         # Define variables to store commands sent by the controller
         self.stop_motion_command = False
-        self.center_image_command = False
-        self.move_to_goal_command = False
-        self.use_force_feedback_command = False
-        
+        self.use_image_feedback_flag = False
+        self.use_pose_feedback_flag = False
+        self.use_force_feedback_flag = False
+
         # Initialize the node
         init_node('robot_control_node')
 
-        # Set publishing rate
-        self.rate = Rate(100)  # hz
-        
         # Create control input subscribers
         Subscriber('/control_error/image_based', TwistStamped, self.image_based_control_error_callback)
         Subscriber('/franka_state_controller/F_ext', WrenchStamped, self.robot_sensed_force_callback)
@@ -64,36 +62,35 @@ class RobotControlNode:
 
         # Create goal state subscribers
         Subscriber('/goal/pose', PoseStamped, self.goal_pose_callback)
-        Subscriber('/goal/force', WrenchStamped, self.goal_force_callback)
-        
-        # Create command subscirbers
-        Subscriber('/command/stop_motion', Bool, self.stop_motion_callback)
-        Subscriber('/command/center_image', Bool, self.center_image_callback)
-        Subscriber('/command/move_to_goal', Bool, self.move_to_goal_callback)
-        Subscriber('/command/use_force_feedback', Bool, self.use_force_feedback_callback)
+        Subscriber('/force_control/set_point', Float64, self.force_set_point_callback)
+
+        # Create command subscribers
+        Subscriber('/command/use_image_feedback', Bool, self.use_image_feedback_command_callback)
+        Subscriber('/command/use_pose_feedback', Bool, self.use_pose_feedback_command_callback)
+        Subscriber('/command/use_force_feedback', Bool, self.use_force_feedback_command_callback)
 
         # Create status publishers
-        self.was_last_goal_reached_status_publisher = Publisher('/status/goal_reached', Bool, queue_size=1)
-        self.current_pose_status_publisher = Publisher('/status/current_pose', PoseStamped, queue_size=1)
+        # self.was_last_goal_reached_status_publisher = Publisher('/status/goal_reached', Bool, queue_size=1)
+        # self.current_pose_status_publisher = Publisher('/status/current_pose', PoseStamped, queue_size=1)
 
         # Create robot cartesian velocity publisher
         self.cartesian_velocity_publisher = Publisher('/arm/cartesian/velocity', TwistStamped, queue_size=1)
 
         # Define a publisher to publish if the image is centered
-        self.image_centered_publisher = Publisher('is_image_centered', Bool, queue_size=1)
+        # self.image_centered_publisher = Publisher('is_image_centered', Bool, queue_size=1)
 
     # Pull control inputs from message
-    def image_based_control_error_callback(self, data: TwistStamped):
+    def image_based_control_error_callback(self, data: TwistStamped) -> None:
 
         # Define the acceptable limits of error for each dimension
         acceptable_errors = [0.005,  # meters in x
-                            1.01,  # meters in y
-                            1.01,  # meters in z
-                            10,  # deg
-                            10,  # deg
-                            10,  # deg
-                            ]
-        
+                             1.01,  # meters in y
+                             1.01,  # meters in z
+                             10,  # deg
+                             10,  # deg
+                             10,  # deg
+                             ]
+
         # Define the control input gains for each dimension
         control_input_gains = [
             # [ k_p, k_d]
@@ -114,42 +111,35 @@ class RobotControlNode:
         current_error[4] = data.twist.angular.y
         current_error[5] = data.twist.angular.z
 
-        # Define a temporary flag to determine if the image is centered
-        temp_is_image_centered = True
-
         # Define a temporary place to store the control inputs
         temp_control_inputs = array([])
 
         # Check if the error is within an acceptable amount
-        for single_dimension_error, gains, single_dimension_acceptable_error in zip(current_error, control_input_gains, acceptable_errors):
-
-            # Determine if the error is acceptable in a single dimension
-            is_dimension_centered = single_dimension_acceptable_error >= abs(single_dimension_error)
+        for single_dimension_error, gains, single_dimension_acceptable_error in zip(current_error, control_input_gains,
+                                                                                    acceptable_errors):
 
             # Calculate the control input for the given dimension
-            if not is_dimension_centered:
-                temp_control_inputs.append(current_error*gains[0])
-            else:
-                temp_control_inputs.append(0)
-
-            # Calculate if the image is centered in all dimensions    
-            temp_is_image_centered = temp_is_image_centered * is_dimension_centered
+            temp_control_inputs = append(temp_control_inputs, current_error * gains[0])
 
         # Save the control input calculated here
         self.image_based_control_input = temp_control_inputs
 
-        # Save the flag for determining if the image is centered
-        self.is_image_centered = temp_is_image_centered
+    def robot_sensed_force_callback(self, data: WrenchStamped) -> None:
+        """
+        Saves the current force felt by the robot and calculates the median force felt by the robot in the recent past.
 
-    # Pull force values felt currently by robot
-    def robot_sensed_force_callback(self, data: WrenchStamped):
+        Parameters
+        ----------
+        data
+            The message containing the force felt by the robot.
+        """
 
         # Pop out an element from each history if too many have been stored
         for dimension, index in zip(self.force_history, arange(6)):
             if len(dimension) >= self.force_history_length:
                 self.force_history[index] = dimension[1:]
 
-        # Add in the the new value
+        # Add in the new value
         # self.force_history[0] = append(self.force_history[0], data.wrench.force.x)
         # self.force_history[1] = append(self.force_history[1], data.wrench.force.y)
         self.force_history[2] = append(self.force_history[2], data.wrench.force.z)
@@ -165,102 +155,99 @@ class RobotControlNode:
         self.robot_sensed_force[4] = 0  # mean(self.force_history[4])
         self.robot_sensed_force[5] = 0  # mean(self.force_history[5])
 
-    # Pull current pose of the robot
-    def robot_current_pose_callback(self, data: FrankaState):
+    def robot_current_pose_callback(self, data: FrankaState) -> None:
+        """
+        Captures the current pose of the robot from messages sent by the robot controller.
+        """
         self.current_pose[0] = data.O_T_EE[3]
-        self.current_pose[1] = data.O_T_EE[7]
-        self.current_pose[2] = data.O_T_EE[11]
-        self.calculate_goal_based_control_input()
-        self.was_last_goal_reached_status_publisher.publish(Bool(
-            sqrt(sum((self.current_pose - self.goal_pose) ** 2)) <= .01  # m
-        ))
-        temp_current_pose = PoseStamped()
-        temp_current_pose.pose.position.x = self.current_pose[0]
-        temp_current_pose.pose.position.y = self.current_pose[1]
-        temp_current_pose.pose.position.z = self.current_pose[2]
-        self.current_pose_status_publisher.publish(temp_current_pose)
+        # self.current_pose[1] = data.O_T_EE[7]
+        # self.current_pose[2] = data.O_T_EE[11]
+        # self.calculate_goal_based_control_input()
+        # self.was_last_goal_reached_status_publisher.publish(Bool(
+        #     sqrt(sum((self.current_pose - self.goal_pose) ** 2)) <= .01  # m
+        # ))
+        # temp_current_pose = PoseStamped()
+        # temp_current_pose.pose.position.x = self.current_pose[0]
+        # temp_current_pose.pose.position.y = self.current_pose[1]
+        # temp_current_pose.pose.position.z = self.current_pose[2]
+        # self.current_pose_status_publisher.publish(temp_current_pose)
 
-    # Pull goal pose from message
     def goal_pose_callback(self, data: PoseStamped):
-        self.goal_pose[0] = data.pose.position.x 
-        self.goal_pose[1] = data.pose.position.y
-        self.goal_pose[2] = data.pose.position.z
+        """
+        Captures the desired pose sent to the node.
+        """
+        self.goal_pose[0] = data.pose.position.x
+        # self.goal_pose[1] = data.pose.position.y
+        # self.goal_pose[2] = data.pose.position.z
         # self.goal_pose[3] = data.twist.angular.x
         # self.goal_pose[4] = data.twist.angular.y
         # self.goal_pose[5] = data.twist.angular.z
 
-    # Pull desired force from message
-    def goal_force_callback(self, data: WrenchStamped):
-        self.goal_force[0] = data.wrench.force.x 
-        self.goal_force[1] = data.wrench.force.y
-        self.goal_force[2] = data.wrench.force.z
-        self.goal_force[3] = data.wrench.torque.x
-        self.goal_force[4] = data.wrench.torque.y
-        self.goal_force[5] = data.wrench.torque.z
+    def force_set_point_callback(self, data: Float64) -> None:
+        """
+        Saves the force set-point to a local variable.
 
-        self.calculate_force_based_control_input()
+        Parameters
+        ----------
+        data
+            The message containing the force set-point to save.
+        """
+        self.goal_force[2] = data.data
 
-    # Pull stop motion command from message
-    def stop_motion_callback(self, data: Bool):
-        self.stop_motion_command = data.data
-
-    # Pull center image command from message
-    def center_image_callback(self, data: Bool):
-        if not self.center_image_command == data.data:
+    def use_image_feedback_command_callback(self, data: Bool) -> None:
+        """
+        Update the use_image_feedback_flag based on the command value given.
+        """
+        if not self.use_image_feedback_flag == data.data:
             if data.data:
                 print("Image feedback turned on.")
             else:
                 print("Image feedback turned off.")
-        self.center_image_command = data.data
+        self.use_image_feedback_flag = data.data
 
-    # Pull move to goal command from message
-    def move_to_goal_callback(self, data: Bool):
-        if not self.move_to_goal_command == data.data:
+    def use_pose_feedback_command_callback(self, data: Bool) -> None:
+        """
+        Update the use_pose_feedback_flag based on the command value given.
+        """
+        if not self.use_pose_feedback_flag == data.data:
             if data.data:
                 print("Moving to goal position.")
             else:
                 print("Not moving to goal.")
-        self.move_to_goal_command = data.data
+        self.use_pose_feedback_flag = data.data
 
-    # Pull use force feedback command from message
-    def use_force_feedback_callback(self, data: Bool):
-        if not self.use_force_feedback_command == data.data:
+    def use_force_feedback_command_callback(self, data: Bool) -> None:
+        """
+        Update the use_force_feedback_flag based on the command value given.
+        """
+        if not self.use_force_feedback_flag == data.data:
             if data.data:
                 print("Force feedback turned on.")
             else:
                 print("Force feedback turned off.")
-        self.use_force_feedback_command = data.data
+        self.use_force_feedback_flag = data.data
 
-    # Calculate the control input based on the goal pose
-    def calculate_goal_based_control_input(self):
-        self.position_based_control_input = self.position_based_gains * (self.current_pose + self.goal_pose)
-
-    # Calculate the control input based on the currently sensed force
-    def calculate_force_based_control_input(self):
-        self.force_based_control_input = self.force_based_gains * (-self.goal_force + self.robot_sensed_force)
-
-
-
-if __name__ == '__main__':
-    
-    node = RobotControlNode()
-    print("Node initialized. Press ctrl+c to terminate.")
-
-    while not is_shutdown():
+    def main(self) -> None:
+        """
+        Calculates the correct control input based on the current error in the system and
+        which control modes are active.
+        """
 
         # Create blank array to store the final control input as an array
         control_input_array = zeros(6)
 
-        if node.move_to_goal_command:
-            control_input_array = control_input_array + node.position_based_control_input
+        if self.use_pose_feedback_flag:
+            control_input_array = control_input_array + (self.position_based_gains *
+                                                         (-self.current_pose + self.goal_pose))
             # print("Goal based control input used.")
 
-        if node.center_image_command:
-            control_input_array = control_input_array + node.image_based_control_input
+        if self.use_image_feedback_flag:
+            control_input_array = control_input_array + self.image_based_control_input
             # print("Image centering control input used.")
 
-        if node.use_force_feedback_command:
-            control_input_array = control_input_array + node.force_based_control_input 
+        if self.use_force_feedback_flag:
+            control_input_array = control_input_array + (self.force_based_gains *
+                                                         (-self.goal_force + self.robot_sensed_force))
             # print("Force feedback control input: ", node.force_based_control_input[2])
 
         # Create a message and fill it with the desired control input
@@ -272,11 +259,22 @@ if __name__ == '__main__':
         control_input_message.twist.angular.y = control_input_array[4]
         control_input_message.twist.angular.z = control_input_array[5]
 
-        node.cartesian_velocity_publisher.publish(control_input_message)
+        self.cartesian_velocity_publisher.publish(control_input_message)
 
-        # Publish if the image is centered
-        node.image_centered_publisher.publish(Bool(node.is_image_centered))
-        
-        # node.was_last_goal_reached_status_publisher.publish(Bool(node.))
-        
-        node.rate.sleep()
+
+if __name__ == '__main__':
+
+    # Create the node
+    node = RobotControlNode()
+
+    # Set publishing rate
+    publishing_rate = Rate(100)  # hz
+
+    print("Node initialized. Press ctrl+c to terminate.")
+
+    while not is_shutdown():
+        # Run the main function of the node
+        node.main()
+
+        # Wait to run the node again
+        publishing_rate.sleep()
